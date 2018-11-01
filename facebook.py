97d63ddf88920b4cc66b638e9a46afef5b5dc8d5
huyn <huyn@cvm6h4zv52.cvm.iastate.edu>
2018-11-01 13:10:55 -0500

    update

--- facebook.py
+++ facebook.py
@@ -398,12 +398,7 @@ def findBiggest(string):
         return "".join(string)
     else:
         return ""
-            
-
-    
-#Given a singly-linked list, print the values in reverse order 
-    
-#Write the partition method for Quicksort  
+             
     
 #Given an array of integers, determine if a subarray exists whose sum is a target value 
 def subArraySum(arr, n, sum): 
@@ -466,14 +461,132 @@ class Link(object):
         print (head.val)
         return head
 #1. 2-SUM in sorted array
-    
+
+#Write the partition method for Quicksort  
       
-#Given a boolean array Cal = [F,F,F,T,T,F,T,T] where T: On vacation F: Not on vacation.
-#Given an integer variable PTO (1)
-#Find the set of days with maximum vacation if PTO were utilised.  
-     
+#Find all triplets with zero sum
+def findTruplets(arr,n):
+    found = False
+    arr.sort()
+    for i in range(0,n-1):
+        # initialize left and right
+        l = i+1
+        r = n-1
+        x = arr[i]
+        while l<r:
+            current = x+arr[l]+arr[r]
+            if (current==0):
+                print (x,arr[l],arr[r])
+                l+=1
+                r-=1
+                found = True
+            elif (current<0):
+                l+=1
+            else:
+                r-=1
+    if found == "False":
+        print ("None")
     
-#Sort an unsorted linked list from smallest to biggest  
-    
-#Checking if Linked List is palindromic  
-    
+def bubbleSort(arr): 
+    n = len(arr) 
+  
+    # Traverse through all array elements 
+    for i in range(n): 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+
+def insertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key
+
+# This function takes last element as pivot, places 
+# the pivot element at its correct position in sorted 
+# array, and places all smaller (smaller than pivot) 
+# to left of pivot and all greater elements to right 
+# of pivot 
+def partition(arr,low,high): 
+    i = ( low-1 )         # index of smaller element 
+    pivot = arr[high]     # pivot 
+  
+    for j in range(low , high): 
+  
+        # If current element is smaller than or 
+        # equal to pivot 
+        if   arr[j] <= pivot: 
+          
+            # increment index of smaller element 
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+  
+# The main function that implements QuickSort 
+# arr[] --> Array to be sorted, 
+# low  --> Starting index, 
+# high  --> Ending index 
+  
+# Function to do Quick sort 
+def quickSort(arr,low,high): 
+    if low < high: 
+  
+        # pi is partitioning index, arr[p] is now 
+        # at right place 
+        pi = partition(arr,low,high) 
+  
+        # Separately sort elements before 
+        # partition and after partition 
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high) 
+        
+def mergeSort(arr): 
+    if len(arr) >1: 
+        mid = len(arr)//2 #Finding the mid of the array 
+        L = arr[:mid] # Dividing the array elements  
+        R = arr[mid:] # into 2 halves 
+  
+        mergeSort(L) # Sorting the first half 
+        mergeSort(R) # Sorting the second half 
+  
+        i = j = k = 0
+          
+        # Copy data to temp arrays L[] and R[] 
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i] 
+                i+=1
+            else: 
+                arr[k] = R[j] 
+                j+=1
+            k+=1
+          
+        # Checking if any element was left 
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+=1
+            k+=1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+=1
+            k+=1
+  
